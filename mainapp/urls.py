140dd42542868b82861c4e10448894a0e2e2f82c
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-06-13 22:53:00 +0300

    setup main page

---
+++ mainapp/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from django.views.decorators.cache import cache_page
+
+import mainapp.views as mainapp
+
+app_name = 'mainapp'
+
+urlpatterns = [
+    path('', mainapp.main, name='index'),
+]
