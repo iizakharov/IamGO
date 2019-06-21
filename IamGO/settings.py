207a6c4130c6b001baf5bd9c48aeade73b999d49
Aleksey Sheludchenkov <aleshkashell@gmail.com>
2019-06-21 16:19:27 +0300

    Prepare for flake8 checking

--- IamGO/settings.py
+++ IamGO/settings.py
@@ -55,8 +55,7 @@ ROOT_URLCONF = 'IamGO.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -124,5 +123,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
-
-
--- mainapp/views.py
+++ mainapp/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render
 
+
 def main(request):
     title = 'Главная'
     context = {
         'title': title
     }
     return render(request, 'mainapp/index.html', context)
-
