24a381dd7d04a32ed67ba5cd48314737e6f89b3b
Ekaterina Ivanova <ivanova.ecaterina@yandex.ru>
2019-06-25 02:11:36 +0300

    Create event models in mainapp

--- mainapp/admin.py
+++ mainapp/admin.py
@@ -1,3 +1,39 @@
-# from django.contrib import admin
+from django.contrib import admin
+from mainapp.models import Event, EventCategory, EventAgent, EventLocation, EventDate, EventGallery
 
-# Register your models here.
+
+class ProductInline(admin.TabularInline):
+    model = Event
+    fields = 'name', 'description'
+    extra = 1
+
+
+@admin.register(EventCategory)
+class EventCategoryAdmin(admin.ModelAdmin):
+    inlines = ProductInline,
+
+
+@admin.register(EventAgent)
+class EventAgentAdmin(admin.ModelAdmin):
+    search_fields = 'name',
+
+
+@admin.register(EventLocation)
+class EventLocationAdmin(admin.ModelAdmin):
+    search_fields = 'name',
+
+
+@admin.register(EventDate)
+class EventDateAdmin(admin.ModelAdmin):
+    search_fields = 'date',
+
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    search_fields = 'name', 'category__name',
+    list_display = 'name', 'category',
+
+
+@admin.register(EventGallery)
+class EventGalleryAdmin(admin.ModelAdmin):
+    search_fields = 'event',
