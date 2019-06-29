dcce360662e3cf0f804b9dc003643523e18fa613
Maria Afanaseva <mashenkachukina@gmail.com>
2019-06-29 18:14:41 +0200

    Added fanctions(get_main_menu, get_event_by_date) in mainapp/views

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,10 +1,23 @@
 from django.shortcuts import render
+from .models import EventCategory, Event
+
+
+def get_main_menu():
+    return EventCategory.objects.filter(is_active=True)
+
+
+def get_event_by_date():
+    return Event.objects.filter(category__is_active=True, is_active=True).order_by('date').select_related('category')
 
 
 def main(request):
     title = 'Главная'
+    main_menu = get_main_menu()
+    event_by_date = get_event_by_date()[:3]
     context = {
-        'title': title
+        'title': title,
+        "main_menu": main_menu,
+        'event_by_date': event_by_date,
     }
     return render(request, 'mainapp/index.html', context)
 
