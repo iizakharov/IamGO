64b1d311552c68eed6516b47c49d3bd19b71d9a0
Ekaterina Ivanova <ivanova.ecaterina@yandex.ru>
2019-07-07 14:08:53 +0300

    Fixed Travis

--- authapp/forms.py
+++ authapp/forms.py
@@ -55,4 +55,3 @@ class UserLoginForm(AuthenticationForm):
         super(UserLoginForm, self).__init__(*args, **kwargs)
         for field_name, field in self.fields.items():
             field.widget.attrs['class'] = 'registration-form-input'
-
