f219fd0e8393b42a9bb56ddb14234d259e403be1
iizakharov <45385589+iizakharov@users.noreply.github.com>
2019-07-09 14:41:25 +0300

    fix

--- mainapp/tests.py
+++ mainapp/tests.py
@@ -11,16 +11,16 @@
 #   def test_view_url_by_name(self):
 #       response = self.client.get(reverse('mainapp:events'))
 #       self.assertEquals(response.status_code, 200)
-#    
+#
 #   def test_view_uses_correct_template(self):
 #       response = self.client.get(reverse('home'))
 #       self.assertEquals(response.status_code, 200)
 #       self.assertTemplateUsed(response, 'home.html')
-#    
+#
 #   def test_home_page_contains_correct_html(self):
 #       response = self.client.get('/')
 #       self.assertContains(response, '<h1>Homepage</h1>')
-#    
+#
 #   def test_home_page_does_not_contain_incorrect_html(self):
 #       response = self.client.get('/')
 #       self.assertNotContains(
