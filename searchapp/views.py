3118970803010bb89bc7f66ecf72e96afcc296af
Ekaterina Ivanova <ivanova.ecaterina@yandex.ru>
2019-07-23 02:40:54 +0300

    created SearchView

--- searchapp/views.py
+++ searchapp/views.py
@@ -1,6 +1,11 @@
 from django.views.generic import ListView
 
-from mainapp.models import Event
+from mainapp.models import Event, EventCategory
+
+
+def get_main_menu():
+
+    return EventCategory.objects.filter(is_active=True).order_by('-name')
 
 
 class SearchView(ListView):
@@ -9,10 +14,13 @@ class SearchView(ListView):
     count = 0
 
     def get_context_data(self, *args, **kwargs):
-        context = super().get_context_data(*args, **kwargs)
-        context['count'] = self.count or 0
-        context['query'] = self.request.GET.get('q')
-        return context
+        my_context = super().get_context_data(*args, **kwargs)
+        my_context['page_title'] = 'Поиск'
+        my_context['main_menu'] = get_main_menu()[:8]
+        my_context['count'] = self.count or 0
+        my_context['query'] = self.request.GET.get('q')
+
+        return my_context
 
     def get_queryset(self):
         request = self.request
@@ -20,9 +28,13 @@ class SearchView(ListView):
 
         if query is not None:
             events_results = Event.objects.search(query)
-            qs = sorted(events_results,
-                        key=lambda instance: instance.pk,
-                        reverse=True)
+            qs = sorted(
+                events_results,
+                key=lambda instance: instance.pk,
+                reverse=True
+            )
             self.count = len(qs)  # since qs is actually a list
+
             return qs
+
         return Event.objects.none()  # just an empty queryset as default
